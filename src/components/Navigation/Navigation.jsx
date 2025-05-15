abda64e5d237f3dc4b9d6150edf95a1836f29067
Inzira <inzira1995@gmail.com>
2025-05-15 13:45:59 -0700

    Fixed BEM classes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,9 +11,9 @@ export default function Navigation({
   setSearchText,
 }) {
   return (
-    <section className="navigation__content">
-      <h1 className="title">What's going on in the world?</h1>
-      <p className="description">
+    <section className="navigation">
+      <h1 className="navigation__title">What's going on in the world?</h1>
+      <p className="navigation__description">
         Find the latest news on any topic and save them in your personal
         account.
       </p>
